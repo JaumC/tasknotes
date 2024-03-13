0182ac6a019c1881ec4c44c0c8a3ffc5ae518adc
JaumC <jvvianachaves@gmail.com>
2024-03-13 11:49:02 -0300

    auteração da home

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,23 @@
 function AbrirM(taskId) {
-    var modal = document.getElementById("modal" + taskId)
-    modal.style.display = 'block'
+    var task = document.getElementById("taskName" + taskId);
+    task.innerText = 'aa';
+
 }
 
 function FecharM(taskId) {
-    var modal = document.getElementById("modal" + taskId)
-    modal.style.display = 'none'
+    var modal = document.getElementById("modal" + taskId);
+    modal.style.display = 'none';
+
+}
+
+function checkIt(id){    
+    const checado = document.getElementById("checked" + id);
+    checado.style.display = 'flex';
+
+}
+
+function uncheckIt(id){    
+    const checado = document.getElementById("checked" + id);
+    checado.style.display = 'none';
+    
 }
