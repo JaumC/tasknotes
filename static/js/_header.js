f2112a7356f557432090fadd0b0d923699b4b116
JaumC <jvvianachaves@gmail.com>
2024-03-19 11:41:08 -0300

    navbar completa

--- static/js/_header.js
+++ static/js/_header.js
@@ -1,5 +1,5 @@
-document.querySelector('a.up_btn').addEventListener('click', function(){
+document.querySelector('div.up_btn').addEventListener('click', function(){
     document.querySelector('.header').classList.toggle('open-upbar');
-
+    document.querySelector('.pai').classList.toggle('open-upbar')
 
 });
