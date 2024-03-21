9f4b776cae31684f8144892dd62aa7bfe5d0615d
JaumC <jvvianachaves@gmail.com>
2024-03-21 12:15:14 -0300

    seila

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,28 @@
+function direito(event, id) {
+    var modal = document.getElementById('rightClick' + id);
+    event.preventDefault();
+    var x = event.clientX;
+    var y = event.clientY;
+
+    var notasElement = document.getElementById('lista' + id);
+    var notasRect = notasElement.getBoundingClientRect();
+    var notasBottom = notasRect.bottom + window.scrollY;
+
+    modal.style.display = 'flex';
+    modal.style.left = (x - 88) + 'px';
+    modal.style.top = notasBottom + 'px';
+
+    // Adiciona um event listener para o clique no documento
+    document.addEventListener('mousedown', function fecharModal(e) {
+        // Verifica se o clique foi fora do modal
+        if (!modal.contains(e.target)) {
+            modal.style.display = 'none';
+            // Remove o event listener após fechar o modal
+            document.removeEventListener('mousedown', fecharModal);
+        }
+    });
+}
+
 function AbrirM(taskId) {
     var lista = document.getElementById("lista" + taskId);
     var modal = document.getElementById("modal" + taskId);
