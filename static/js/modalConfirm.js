2e6d1c8d2fdbb15426f8667ee0a38ba7ab1cf0b8
JaumC <jvvianachaves@gmail.com>
2024-04-04 09:11:22 -0300

    modalClose implementado

--- static/js/modalConfirm.js
+++ static/js/modalConfirm.js
@@ -4,10 +4,19 @@ function modalConfirm(id){
 
     modalConfirm.style.display = 'flex';
     rightClick.style.display = 'none';
-    
+
+    //Função pra fechar o modal
+    document.addEventListener('mousedown', function fecharModal(e) {
+        if (!modalConfirm.contains(e.target)) {
+            modalConfirm.style.display = 'none';
+            document.removeEventListener('mousedown', fecharModal);
+        }
+    });
+
 }
 
 function modalConfirmFechar(id){
-    const modalConfirm = document.getElementById('modalConfirm' + id);
-    modalConfirm.style.display = 'none';
+    document.getElementById('modalConfirm' + id).style.display = 'none';
+
 }
+
